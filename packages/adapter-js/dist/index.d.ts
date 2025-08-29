5d43ee9bc414053cafb1d6de11b92b8d94dbf8be
chenpingfromgxu <127575734+chenpingfromgxu@users.noreply.github.com>
2025-08-29 19:48:21 +0800

    build: 构建adapter-js包

---
+++ packages/adapter-js/dist/index.d.ts
@@ -0,0 +1,7 @@
+interface SdkH5 {
+    captureError(error: unknown, attributes?: Record<string, any>): void;
+    log(level: string, message: string, attributes?: Record<string, any>): void;
+}
+declare function installAutoCapture(sdk: SdkH5): void;
+
+export { type SdkH5, installAutoCapture };
